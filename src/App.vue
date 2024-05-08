9148db4499a028b96bf4e326d445122102a875ee
Brandon Humboldt <brhumboldt@gmail.com>
2024-05-08 11:48:03 -0500

    fetch and display users

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script>
 // https://pokeapi.co/api/v2/pokemon?limit=151
 
+import UserPage from '@/components/UserPage.vue'
+
 export default {
+  components: { UserPage },
   data: () => ({
     pokedex: [1, 2, 3]
   }),
@@ -18,11 +21,11 @@ export default {
 }
 </script>
 
+<!-- use @click.prevent to prevent default event behavior -->
 <template>
   <h1>New App</h1>
 
-  <pre>{{ pokedex }}</pre>
-  <button @click="fetchPokemon">Fetch Pokemon</button>
+  <user-page></user-page>
 </template>
 
 <style></style>
---
+++ src/components/UserPage.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data: () => ({
+    users: []
+  }),
+  methods: {
+    async fetchUsers() {
+      this.users = await fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+        res.json()
+      )
+    }
+  },
+  created() {
+    this.fetchUsers()
+  }
+}
+</script>
+
+<template>
+  <div v-for="user in users" :key="user.id">
+    <h2>{{ user.name }}</h2>
+    <p>{{ user.email }}</p>
+  </div>
+</template>
+
+<style></style>
