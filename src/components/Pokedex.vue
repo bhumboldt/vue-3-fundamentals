6b316f98c8119e79dbbf205f824af24c82866ff2
Brandon Humboldt <brhumboldt@gmail.com>
2024-05-08 12:10:03 -0500

    reactive, ref and computed uses in composition api

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,7 +1,17 @@
 <script>
+import { ref, computed, reactive } from 'vue'
+
 export default {
   async setup() {
-    const regionName = 'kanto'
+    const regionName = ref('kanto')
+    // You can use computedProperties instead, but it's good to be explicit
+    // const regionNameAllCaps = computed(() => regionName.value.toUpperCase())
+
+    const state = reactive({
+      elemntType: 'lightning'
+    })
+
+    const regionNameAllCaps = computed(() => state.elemntType.toUpperCase())
 
     const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((res) =>
       res.json()
@@ -9,12 +19,21 @@ export default {
 
     return {
       pokedex,
-      regionName
+      regionName,
+      regionNameAllCaps
+    }
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = 'johto'
     }
   }
 }
 </script>
 
 <template>
+  <h2>{{ regionNameAllCaps }}</h2>
+  <h3>{{ regionName }}</h3>
+  <button @click="changeRegionName">Change Region</button>
   <pre>{{ pokedex }}</pre>
 </template>
