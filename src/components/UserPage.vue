5ed191131649a3c396124034024aa14d84945d83
Brandon Humboldt <brhumboldt@gmail.com>
2024-05-08 12:16:41 -0500

    different way of using composition api for users page

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,19 +1,31 @@
 <script>
+import { reactive } from 'vue'
+
 export default {
   async setup() {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
-      res.json()
-    )
+    const state = reactive({
+      users: []
+    })
+
+    async function fetchUsers() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+        res.json()
+      )
+
+      return response
+    }
+
+    state.users = await fetchUsers()
 
     return {
-      users
+      state
     }
   }
 }
 </script>
 
 <template>
-  <div v-for="user in users" :key="user.id">
+  <div v-for="user in state.users" :key="user.id">
     <h2>{{ user.name }}</h2>
     <p>{{ user.email }}</p>
   </div>
